2d072ab558a4f3dec64c61c322f2be27a23abbf1
priyeshbhawsar1 <priyesh.jn786@gmail.com>
2022-01-25 18:02:28 +0530

    resource file added and hide password

--- src/main/java/Dao/Mail.java
+++ src/main/java/Dao/Mail.java
@@ -3,10 +3,13 @@ package Dao;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
 
 public class Mail {
-    public static void main(String[] args) throws MessagingException {
+    public static void main(String[] args) {
         System.out.println("prepararing send to message");
         String message="Hello,thanks for Register ";
         String subject="Well com Profile ";
@@ -17,7 +20,7 @@ public class Mail {
         sentEmail(message,subject,to,from);
     }
         //this is responsible  to send email
-        private static void sentEmail(String message, String subject, String to, String from) throws MessagingException {
+        private static void sentEmail(String message, String subject, String to, String from)  {
         //variable for gmail
         String host = "smtp.gmail.com";
         //get the syatem.properties
@@ -29,12 +32,27 @@ public class Mail {
         properties.put("mail.smtp.port", "465"); //port key value pair
         properties.put("mail.smtp.ssl.enable", "true"); //ssl enable key value pair
         properties.put("mail.smtp.auth", "true"); // auth enable key value pair
+            FileInputStream fis= null;
+            try {
+                fis = new FileInputStream("resources/config.properties");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            Properties p= new Properties();
+            try {
+                p.load(fis);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String username=p.getProperty("username");
+            String password=p.getProperty("password");
+            System.out.println(username);
 
         //step1: to get the session object..
         Session session = Session.getInstance(properties, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("priyesh.bhawsar@hotwaxsystems.com", "Shrinatraj786@");
+                return new PasswordAuthentication("username", "password");
             }
         });
         session.setDebug(true);
